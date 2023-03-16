aa32f2f107c40fc98dd907f7f6332a35e7a4fe37
Michael Simon <michaelsimonaz@gmail.com>
2023-03-16 11:53:59 -0700

    instructions wording

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ export default function App() {
       </h1>
       <ol>
         <li>Enter some text</li>
-        <li>Click the copy button to copy text</li>
-        <li>Click the delete button to delete the input</li>
-        <li>Click the add button to add a new input</li>
+        <li>Click the copy to copy text</li>
+        <li>Click the delete to delete the input</li>
+        <li>Click "add input" to add a new input</li>
         <li>
-          Fields and values are automatically updated in the URL parameters for
-          easy sharing.
+          Fields and values are automatically updated in URL parameters for easy
+          sharing
         </li>
       </ol>
       <Pasta />
