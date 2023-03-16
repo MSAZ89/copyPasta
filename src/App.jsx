354a5abfe1a4319877a810a2fbd6c6588af30ef0
Michael Simon <michaelsimonaz@gmail.com>
2023-03-16 12:09:36 -0700

    spelling lol

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ export default function App() {
       </h1>
       <ol>
         <li>Enter some text</li>
-        <li>Click the copy to copy text</li>
-        <li>Click the delete to delete the input</li>
+        <li>Click copy to copy text</li>
+        <li>Click delete to delete the input</li>
         <li>Click "add input" to add a new input</li>
         <li>
           Fields and values are automatically updated in URL parameters for easy
