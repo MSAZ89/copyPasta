09cf5c2d3eb4c752175f0b9341ea2f3be13411c4
Michael Simon <michaelsimonaz@gmail.com>
2023-03-16 11:51:49 -0700

    credits

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,19 @@ import Pasta from "./Pasta";
 export default function App() {
   return (
     <div className="App">
-      <h1>Copy Pasta</h1>
+      <h1>
+        Copy Pasta <span style={{ fontSize: 18 }}>by Michael Simon</span>
+      </h1>
+      <ol>
+        <li>Enter some text</li>
+        <li>Click the copy button to copy text</li>
+        <li>Click the delete button to delete the input</li>
+        <li>Click the add button to add a new input</li>
+        <li>
+          Fields and values are automatically updated in the URL parameters for
+          easy sharing.
+        </li>
+      </ol>
       <Pasta />
     </div>
   );
