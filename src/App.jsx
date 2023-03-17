2fb6e34e3feba35e9fd28bc8b65f56d95815c1e7
Michael Simon AZ <michaelsimonaz@gmail.com>
2023-03-16 20:06:31 -0700

    info tab bottom change

--- src/App.jsx
+++ src/App.jsx
@@ -14,27 +14,26 @@ export default function App() {
           </a>
         </span>
       </h1>
-      <details>
-        <summary>Info</summary>
-        <ol className="my-4 list-disc text-sm">
-          <li>Enter some text</li>
-          <li>Click copy to copy text</li>
-          <li>Click delete to delete the input</li>
-          <li>Click "add input" to add a new input</li>
-          <li>
-            Fields and values are automatically updated in URL parameters for
-            easy sharing
-          </li>
-        </ol>
-      </details>
-
       <Pasta />
 
       <div className="my-8">
-        <p className="font-light mb-4 text-sm">
-          No data is collected on this page in any form, only URL params. This
-          is a simple tool to copy text to clipboard easily.
-        </p>
+        <details>
+          <summary>Info</summary>
+          <ol className="my-4 list-disc text-sm">
+            <li>Enter some text</li>
+            <li>Click copy to copy text</li>
+            <li>Click delete to delete the input</li>
+            <li>Click "add input" to add a new input</li>
+            <li>
+              Fields and values are automatically updated in URL parameters for
+              easy sharing
+            </li>
+          </ol>
+          <p className="font-light mb-4 text-lg">
+            No data is collected on this page in any form, only URL params. This
+            is a simple tool to copy text to clipboard easily.
+          </p>
+        </details>
       </div>
     </div>
   );
